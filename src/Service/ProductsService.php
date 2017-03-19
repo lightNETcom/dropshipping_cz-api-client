499b45ed05627aa543ffc59afe69e701193d8dbb
Jan Cinert <jan.cinert@gmail.com>
2017-03-19 06:23:52 +0100

    Added products/categories

--- src/Service/ProductsService.php
+++ src/Service/ProductsService.php
@@ -44,4 +44,38 @@ class ProductsService extends ServiceBase
 
         return $list;
     }
+    
+    /**
+     * @param int $eshopId
+     * @param int|null $limit
+     * @param int|null $offset
+     *
+     * @return array|Error
+     */
+    public function getCategories($eshopId, $limit = null, $offset = null)
+    {
+        $params = array(
+            'eshop_id' => $eshopId,
+            'limit' => $limit,
+            'offset' => $offset,
+        );
+
+        $response = $this->apiClient->sendRequest(
+            'GET',
+            'products/categories',
+            $params
+        );
+
+        if (!isset($response['data'])) {
+            return new Error($response);
+        }
+
+        $list = array();
+
+        foreach ($response['data'] as $item) {
+            $list[] = $item;
+        }
+
+        return $list;
+    }
 }
